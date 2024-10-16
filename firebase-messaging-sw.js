4b4e0a4ca259bb8c95af7a457b9a1eb2c8a20662
Mario <48096472+Hezychasta@users.noreply.github.com>
2024-10-16 19:34:27 +0200

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js");
+
+var firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log("Received background message ", payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
